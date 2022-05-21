fc27a2ddd3a344191ad2ec3ca05fbc8761a13935
TawfiqImamRafi <tawfiqrafi@gmail.com>
2022-05-21 12:33:36 +0600

    vimeo install

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        Vimeo\Laravel\VimeoServiceProvider::class
 
     ],
 
@@ -212,6 +213,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Vimeo' => Vimeo\Laravel\Facades\Vimeo::class
     ])->toArray(),
 
 ];
